4d6c15fee04dd6ac5045c5a294422df53831849f
Anton <anton.gap99@gmail.com>
2022-07-12 20:04:25 +0300

    some changes

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -2,30 +2,9 @@
 /* eslint-disable eol-last */
 'use strict';
 
-const data = [
-  {
-    name: 'Иван',
-    surname: 'Петров',
-    phone: '+79514545454',
-  },
-  {
-    name: 'Егор',
-    surname: 'Южаков',
-    phone: '+79999999999',
-  },
-  {
-    name: 'Семён',
-    surname: 'Абрамов',
-    phone: '+79800252525',
-  },
-  {
-    name: 'Мария',
-    surname: 'Ширяева',
-    phone: '+79876543210',
-  },
-];
 {
 /* применить setStorage вместо addContactData*/
+
 
   const getStorage = (key) => {
     let result;
